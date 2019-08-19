2910287f793bfecfb882e600f3567559b60e5c46
Mr. T <austinbook@Christophers-MacBook-Pro.local>
2019-08-18 21:03:36 -0600

    Updated protfolio

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCrt1kGtsmPcfp5SgSv-F_88nI7tfc4NU4",
+  authDomain: "portfolio-36adc.firebaseapp.com",
+  databaseURL: "https://portfolio-36adc.firebaseio.com",
+  projectId: "portfolio-36adc",
+  storageBucket: "portfolio-36adc.appspot.com",
+  messagingSenderId: "563851574137",
+  appId: "1:563851574137:web:80e63ac2ae730864"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+$("#contact-submit").on("click", function() {
+  var name = $("#nameInput");
+  var email = $("#emailInput");
+  var message = $("#subject");
+
+  database.ref("/contactData").set({
+    name: name,
+    email: email,
+    message: message
+  });
+});
